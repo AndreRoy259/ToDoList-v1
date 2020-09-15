aebeba1989a362afcea7d65ae1fd45e7afe1fe7d
André Roy <andreroy25.9@gmail.com>
2020-09-14 22:17:10 -0400

    [ADD] v2 with MongoDB

--- app.js
+++ app.js
@@ -1,21 +1,78 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 3000;
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
-
 app.set("view engine", "ejs");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  let day = date.getDate();
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-  res.render("list", { listTitle: day, newListItems: items });
+const itemsSchema = {
+  name: String,
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Buy Food",
+});
+
+const item2 = new Item({
+  name: "Cook Food",
+});
+
+const item3 = new Item({
+  name: "Eat Food",
+});
+
+const defaultItems = [item1, item2, item3];
+
+app.get("/", (req, res) => {
+  Item.find({}, (err, foundItems) => {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully saved default items to DB.");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", { listTitle: "Today", newListItems: foundItems });
+    }
+  });
+});
+
+app.post("/", (req, res) => {
+  const itemName = req.body.newItem;
+
+  const item = new Item({
+    name: itemName,
+  });
+
+  item.save();
+  res.redirect("/");
+});
+
+app.post("/delete", (req, res) => {
+  const checkedItemId = req.body.checkbox;
+  Item.findByIdAndRemove(checkedItemId, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Successfully deleted check item.");
+      res.redirect("/");
+    }
+  });
 });
 
 app.get("/work", (req, res) => {
@@ -24,24 +81,6 @@ app.get("/work", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.render("about");
-});
-
-app.post("/work", (req, res) => {
-  const item = req.body.newItem;
-  workItems.push(item);
-  res.redirect("/work");
-});
-
-app.post("/", (req, res) => {
-  const item = req.body.newItem;
-
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
 });
 
 app.listen(port, () => {
