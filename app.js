9b14ea6909f2b63ec4627054c13af38e1702d87a
André Roy <andreroy25.9@gmail.com>
2020-09-18 21:56:34 -0400

    [ADD] MongoDB Atlas Support

--- app.js
+++ app.js
@@ -11,10 +11,13 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  "mongodb+srv://admin-andre:Test123@cluster0.debee.mongodb.net/todolistDB?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const itemsSchema = {
   name: String,
